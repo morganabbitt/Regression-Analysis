ad0969622d3ce3dc9695384c545b26838c0a53cb
Walker Stevens <walkerstevens@ymail.com>
2020-01-17 15:42:37 -0800

    new plots

--- modeling.py
+++ modeling.py
@@ -32,9 +32,10 @@ data = data[data['MachineHoursCurrentMeter']<np.quantile(data['MachineHoursCurre
 hours_mean = np.mean(data['MachineHoursCurrentMeter'])
 y = data['SalePrice']
 X = data[['MachineHoursCurrentMeter']]
-model = sm.OLS(y, X)
 X = sm.add_constant(X)
+model = sm.OLS(y, X)
 results = model.fit()
+const, hours = results.params
 plt.scatter(X['MachineHoursCurrentMeter'],y,alpha=0.1)
 plt.plot(X['MachineHoursCurrentMeter'],X['MachineHoursCurrentMeter'].map(lambda x: const + x*hours),color='r')
 plt.title('Usage Price Model')
@@ -43,8 +44,8 @@ plt.ylabel('Log(Sale Price)')
 #%%
 data = pd.read_csv("C:/Users/walke/Documents/galvanize/case_studies/regression-case-study/data/train.csv")
 data['SalePrice'] = np.log(data['SalePrice'])
+data = data[data['YearMade']!=1000]
 #data['MachineHoursCurrentMeter'][data['MachineHoursCurrentMeter']!=0 & ~ data['MachineHoursCurrentMeter'].isnull()] = np.log(data['MachineHoursCurrentMeter'][data['MachineHoursCurrentMeter']!=0 & ~ data['MachineHoursCurrentMeter'].isnull()])
-data = data[data['YearMade']>1940]
 data['MachineHoursCurrentMeter'][data['MachineHoursCurrentMeter']==0] = hours_mean
 data['MachineHoursCurrentMeter'][data['MachineHoursCurrentMeter'].isnull()] = hours_mean
 data = data[data['MachineHoursCurrentMeter']<hours_threshold]
@@ -53,11 +54,38 @@ X = data[['MachineHoursCurrentMeter']]
 X = sm.add_constant(X)
 model = sm.OLS(y, X)
 results = model.fit()
+const, hours = results.params
 plt.scatter(X['MachineHoursCurrentMeter'],y,alpha=0.1)
 plt.plot(X['MachineHoursCurrentMeter'],X['MachineHoursCurrentMeter'].map(lambda x: const + x*hours),color='r')
 plt.title('Usage Price Model')
 plt.xlabel('Machine Hours')
 plt.ylabel('Log(Sale Price)')
 #%%
-data = pd.read_csv("C:/Users/walke/Documents/galvanize/case_studies/regression-case-study/data/train.csv")
-data['SalePrice'] = np.log(data['SalePrice'])
+data['ProductSize'] = data['ProductSize'].fillna('Compact').replace({'Mini':0, 'Compact':1, 'Small': 2, 'Medium':3, 'Large':5, 'Large / Medium':4})
+
+data["Age"] = (data["saledate"].map(lambda x: int(x.split(" ")[0][-4:])))-data["YearMade"]
+
+final_train = data[['SalePrice','Age','MachineHoursCurrentMeter','ProductSize']]
+y = final_train['SalePrice']
+X = final_train[['Age','MachineHoursCurrentMeter','ProductSize']]
+X = sm.add_constant(X)
+model = sm.OLS(y, X)
+results = model.fit()
+coeffs = results.params
+
+"""X_new = sm.add_constant(data['ProductSize'])
+y_new = data['SalePrice']
+model_new = sm.OLS(y_new, X_new)
+results_new = model_new.fit()
+results_new.summary()
+fig, ax = plt.subplots(figsize=(15,10))
+ax.scatter(y_new, results_new.resid, alpha=0.01)
+plt.axhline(0, linestyle='--',color='r')
+plt.title('Residuals of Numeric Rank of Product Size', size=25)
+plt.xlabel('Log Price', size=20)
+plt.ylabel('Residuals')
+
+const, size = results_new.params
+fig, ax = plt.subplots(figsize=(15,10))
+plt.scatter(data['ProductSize'], data['SalePrice'], alpha=0.002, s=120)
+plt.plot(data['ProductSize'], data['ProductSize'].map(lambda x: const + size * x), color='r', linestyle='--', linewidth=5)"""
